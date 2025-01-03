fddab17c63cd0781a1148f64339c64211368e315
andreiprb <andreipriboi@icloud.com>
2025-01-03 23:08:54 +0200

    made work on mac

---
+++ ViewOBJModel-main/ViewOBJModel-main/ViewOBJModel-main/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ViewOBJModel-main
+//
+//  Created by Andrei PRIBOI on 03.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
