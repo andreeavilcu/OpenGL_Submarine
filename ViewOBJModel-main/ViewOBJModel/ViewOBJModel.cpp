7273ec429233ccca3cc1bc1375143edee2093a4c
Tudose <vasile.tudose@student.unitbv.ro>
2025-01-03 16:14:46 +0200

    am modificat mainul si am pus camera intr-o clasa separata

--- ViewOBJModel-main/ViewOBJModel/ViewOBJModel.cpp
+++ ViewOBJModel-main/ViewOBJModel/ViewOBJModel.cpp
@@ -27,9 +27,13 @@
 #pragma comment (lib, "glew32.lib")
 #pragma comment (lib, "OpenGL32.lib")
 
+#include "Camera.h"
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+Camera* camera = new Camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0, 0.0, 3.0));
 
 enum ECameraMovementType
 {
@@ -42,210 +46,12 @@ enum ECameraMovementType
 	DOWN
 };
 
-class Camera
-{
-private:
-	// Default camera values
-	const float zNEAR = 0.1f;
-	const float zFAR = 500.f;
-	const float YAW = -90.0f;
-	const float PITCH = 0.0f;
-	const float FOV = 45.0f;
-	glm::vec3 startPosition;
-
-public:
-	Camera(const int width, const int height, const glm::vec3& position)
-	{
-		startPosition = position;
-		Set(width, height, position);
-	}
-
-	void Set(const int width, const int height, const glm::vec3& position)
-	{
-		this->isPerspective = true;
-		this->yaw = YAW;
-		this->pitch = PITCH;
-
-		this->FoVy = FOV;
-		this->width = width;
-		this->height = height;
-		this->zNear = zNEAR;
-		this->zFar = zFAR;
-
-		this->worldUp = glm::vec3(0, 1, 0);
-		this->position = position;
-
-		lastX = width / 2.0f;
-		lastY = height / 2.0f;
-		bFirstMouseMove = true;
-
-		UpdateCameraVectors();
-	}
-
-	void Reset(const int width, const int height)
-	{
-		Set(width, height, startPosition);
-	}
-
-	void Reshape(int windowWidth, int windowHeight)
-	{
-		width = windowWidth;
-		height = windowHeight;
-
-		// define the viewport transformation
-		glViewport(0, 0, windowWidth, windowHeight);
-	}
-
-	const glm::mat4 GetViewMatrix() const
-	{
-		// Returns the View Matrix
-		return glm::lookAt(position, position + forward, up);
-	}
-
-	const glm::vec3 GetPosition() const
-	{
-		return position;
-	}
-
-	const glm::mat4 GetProjectionMatrix() const
-	{
-		glm::mat4 Proj = glm::mat4(1);
-		if (isPerspective) {
-			float aspectRatio = ((float)(width)) / height;
-			Proj = glm::perspective(glm::radians(FoVy), aspectRatio, zNear, zFar);
-		}
-		else {
-			float scaleFactor = 2000.f;
-			Proj = glm::ortho<float>(
-				-width / scaleFactor, width / scaleFactor,
-				-height / scaleFactor, height / scaleFactor, -zFar, zFar);
-		}
-		return Proj;
-	}
-
-	void ProcessKeyboard(ECameraMovementType direction, float deltaTime)
-	{
-		float velocity = (float)(cameraSpeedFactor * deltaTime);
-		switch (direction) {
-		case ECameraMovementType::FORWARD:
-			position += forward * velocity;
-			break;
-		case ECameraMovementType::BACKWARD:
-			position -= forward * velocity;
-			break;
-		case ECameraMovementType::LEFT:
-			position -= right * velocity;
-			break;
-		case ECameraMovementType::RIGHT:
-			position += right * velocity;
-			break;
-		case ECameraMovementType::UP:
-			position += up * velocity;
-			break;
-		case ECameraMovementType::DOWN:
-			position -= up * velocity;
-			break;
-		}
-	}
-
-	void MouseControl(float xPos, float yPos)
-	{
-		if (bFirstMouseMove) {
-			lastX = xPos;
-			lastY = yPos;
-			bFirstMouseMove = false;
-		}
-
-		float xChange = xPos - lastX;
-		float yChange = lastY - yPos;
-		lastX = xPos;
-		lastY = yPos;
-
-		if (fabs(xChange) <= 1e-6 && fabs(yChange) <= 1e-6) {
-			return;
-		}
-		xChange *= mouseSensitivity;
-		yChange *= mouseSensitivity;
-
-		ProcessMouseMovement(xChange, yChange);
-	}
-
-	void ProcessMouseScroll(float yOffset)
-	{
-		if (FoVy >= 1.0f && FoVy <= 90.0f) {
-			FoVy -= yOffset;
-		}
-		if (FoVy <= 1.0f)
-			FoVy = 1.0f;
-		if (FoVy >= 90.0f)
-			FoVy = 90.0f;
-	}
-
-private:
-	void ProcessMouseMovement(float xOffset, float yOffset, bool constrainPitch = true)
-	{
-		yaw += xOffset;
-		pitch += yOffset;
-
-		//std::cout << "yaw = " << yaw << std::endl;
-		//std::cout << "pitch = " << pitch << std::endl;
-
-		// Avem grijã sã nu ne dãm peste cap
-		if (constrainPitch) {
-			if (pitch > 89.0f)
-				pitch = 89.0f;
-			if (pitch < -89.0f)
-				pitch = -89.0f;
-		}
-
-		// Se modificã vectorii camerei pe baza unghiurilor Euler
-		UpdateCameraVectors();
-	}
-
-	void UpdateCameraVectors()
-	{
-		// Calculate the new forward vector
-		this->forward.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-		this->forward.y = sin(glm::radians(pitch));
-		this->forward.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-		this->forward = glm::normalize(this->forward);
-		// Also re-calculate the Right and Up vector
-		right = glm::normalize(glm::cross(forward, worldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-		up = glm::normalize(glm::cross(right, forward));
-	}
-
-protected:
-	const float cameraSpeedFactor = 250.0f;
-	const float mouseSensitivity = 0.1f;
-
-	// Perspective properties
-	float zNear;
-	float zFar;
-	float FoVy;
-	int width;
-	int height;
-	bool isPerspective;
-
-	glm::vec3 position;
-	glm::vec3 forward;
-	glm::vec3 right;
-	glm::vec3 up;
-	glm::vec3 worldUp;
-
-	// Euler Angles
-	float yaw;
-	float pitch;
-
-	bool bFirstMouseMove = true;
-	float lastX = 0.f, lastY = 0.f;
-};
 
 GLuint ProjMatrixLocation, ViewMatrixLocation, WorldMatrixLocation;
-Camera* pCamera = nullptr;
 
 void Cleanup()
 {
-	delete pCamera;
+
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -258,6 +64,7 @@ double lastFrame = 0.0f;
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
+
 	if (key == GLFW_KEY_A && action == GLFW_PRESS) {
 
 	}
@@ -266,22 +73,22 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 		glfwSetWindowShouldClose(window, true);
 
 	if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(FORWARD, (float)deltaTime);
+		camera->ProcessKeyboard(FORWARD, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(BACKWARD, (float)deltaTime);
+		camera->ProcessKeyboard(BACKWARD, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(LEFT, (float)deltaTime);
+		camera->ProcessKeyboard(LEFT, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(RIGHT, (float)deltaTime);
+		camera->ProcessKeyboard(RIGHT, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(UP, (float)deltaTime);
+		camera->ProcessKeyboard(UP, (float)deltaTime);
 	if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
-		pCamera->ProcessKeyboard(DOWN, (float)deltaTime);
+		camera->ProcessKeyboard(DOWN, (float)deltaTime);
 
 	if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
 		int width, height;
 		glfwGetWindowSize(window, &width, &height);
-		pCamera->Reset(width, height);
+		camera->Reset(width, height);
 
 	}
 }
@@ -387,7 +194,7 @@ int main()
 	glEnableVertexAttribArray(0);
 
 	// Create camera
-	pCamera = new Camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0, 0.0, 3.0));
+	camera = new Camera(SCR_WIDTH, SCR_HEIGHT, glm::vec3(0.0, 0.0, 3.0));
 
 	glm::vec3 lightPos(0.0f, 2.0f, 1.0f);
 	glm::vec3 cubePos(0.0f, 5.0f, 1.0f);
@@ -437,10 +244,10 @@ int main()
 		lightingShader.SetVec3("objectColor", 0.5f, 1.0f, 0.31f);
 		lightingShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		lightingShader.SetVec3("lightPos", lightPos);
-		lightingShader.SetVec3("viewPos", pCamera->GetPosition());
+		lightingShader.SetVec3("viewPos", camera->GetPosition());
 
-		lightingShader.setMat4("projection", pCamera->GetProjectionMatrix());
-		lightingShader.setMat4("view", pCamera->GetViewMatrix());
+		lightingShader.setMat4("projection", camera->GetProjectionMatrix());
+		lightingShader.setMat4("view", camera->GetViewMatrix());
 
 		// render the model
 		glm::mat4 model = glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
@@ -453,12 +260,12 @@ int main()
 		lightingWithTextureShader.SetVec3("objectColor", 0.5f, 1.0f, 0.31f);
 		lightingWithTextureShader.SetVec3("lightColor", 1.0f, 1.0f, 1.0f);
 		lightingWithTextureShader.SetVec3("lightPos", lightPos);
-		lightingWithTextureShader.SetVec3("viewPos", pCamera->GetPosition());
+		lightingWithTextureShader.SetVec3("viewPos", camera->GetPosition());
 		lightingWithTextureShader.setInt("texture_diffuse1", 0);
 
 
-		lightingWithTextureShader.setMat4("projection", pCamera->GetProjectionMatrix());
-		lightingWithTextureShader.setMat4("view", pCamera->GetViewMatrix());
+		lightingWithTextureShader.setMat4("projection", camera->GetProjectionMatrix());
+		lightingWithTextureShader.setMat4("view", camera->GetViewMatrix());
 		glm::mat4 piratModel = glm::scale(glm::mat4(1.0), glm::vec3(1.f));
 		lightingWithTextureShader.setMat4("model", piratModel);
 		piratObjModel.Draw(lightingWithTextureShader);
@@ -475,8 +282,8 @@ int main()
 
 		// also draw the lamp object
 		lampShader.use();
-		lampShader.setMat4("projection", pCamera->GetProjectionMatrix());
-		lampShader.setMat4("view", pCamera->GetViewMatrix());
+		lampShader.setMat4("projection", camera->GetProjectionMatrix());
+		lampShader.setMat4("view", camera->GetViewMatrix());
 		glm::mat4 lightModel = glm::translate(glm::mat4(1.0), lightPos);
 		lightModel = glm::scale(lightModel, glm::vec3(0.05f)); // a smaller cube
 		lampShader.setMat4("model", lightModel);
@@ -506,15 +313,15 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	// make sure the viewport matches the new window dimensions; note that width and 
 	// height will be significantly larger than specified on retina displays.
-	pCamera->Reshape(width, height);
+	camera->Reshape(width, height);
 }
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
-	pCamera->MouseControl((float)xpos, (float)ypos);
+	camera->MouseControl((float)xpos, (float)ypos);
 }
 
 void scroll_callback(GLFWwindow* window, double xoffset, double yOffset)
 {
-	pCamera->ProcessMouseScroll((float)yOffset);
+	camera->ProcessMouseScroll((float)yOffset);
 }
